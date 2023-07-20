091a359bcb4f369d61652613c29fd429f5c83261
SakshiRathod13 <sakshirathod84352@gmail.com>
2023-07-21 00:09:44 +0530

    initial changes

--- flutter_p/lib/main.dart
+++ flutter_p/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(title: "app", home: HomePage()));
+  runApp(MaterialApp(title: "MyApp", home: HomePage()));
 }
 
 class HomePage extends StatelessWidget {
@@ -11,11 +11,31 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("hello"),
-        backgroundColor: Colors.amber,
+        title: Text("Flutter"),
+        backgroundColor: Colors.teal,
       ),
-      body: Container(
-        child: Text("hello"),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              color: Colors.redAccent,
+              width: 100,
+              height: 100,
+            ),
+            Container(
+              color: Colors.green,
+              width: 100,
+              height: 100,
+            ),
+            Container(
+              color: Colors.teal,
+              width: 100,
+              height: 100,
+            )
+          ],
+        ),
       ),
     );
   }
