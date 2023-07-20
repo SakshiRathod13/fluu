276c2a50ed2cdc58eb7f52e21a829cf8cee0db49
SakshiRathod13 <sakshirathod84352@gmail.com>
2023-07-21 01:16:06 +0530

    initial changes

--- flutter_p/lib/main.dart
+++ flutter_p/lib/main.dart
@@ -10,33 +10,45 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Flutter"),
-        backgroundColor: Colors.teal,
-      ),
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              color: Colors.redAccent,
-              width: 100,
-              height: 100,
-            ),
-            Container(
-              color: Colors.green,
-              width: 100,
-              height: 100,
-            ),
-            Container(
-              color: Colors.teal,
-              width: 100,
-              height: 100,
-            )
-          ],
+        appBar: AppBar(
+          title: Text(
+            "Flutter",
+            textAlign: TextAlign.center,
+          ),
+          backgroundColor: Colors.teal,
         ),
-      ),
-    );
+        body: Container(),
+        drawer: Drawer(
+            child: ListView(
+          padding: EdgeInsets.all(0),
+          children: [
+            UserAccountsDrawerHeader(
+                accountName: Text("Sakshi Rathod"),
+                accountEmail: Text("sakshirathod###@gmail.com"),
+                currentAccountPicture: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        "https://images.unsplash.com/photo-1525431836161-e40d6846e656?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c3BsYXNofGVufDB8fDB8fHww&auto=format&fit=crop&w=600&q=60"))),
+            ListTile(
+                leading: Icon(Icons.person),
+                title: Text("Personal"),
+                subtitle: Text("Sakshi rathod"),
+                trailing: Icon(Icons.edit)),
+            ListTile(
+                leading: Icon(Icons.person),
+                title: Text("Personal"),
+                subtitle: Text("Sakshi rathod"),
+                trailing: Icon(Icons.edit)),
+            ListTile(
+                leading: Icon(Icons.person),
+                title: Text("Personal"),
+                subtitle: Text("Sakshi rathod"),
+                trailing: Icon(Icons.edit)),
+            ListTile(
+                leading: Icon(Icons.person),
+                title: Text("Personal"),
+                subtitle: Text("Sakshi rathod"),
+                trailing: Icon(Icons.edit)),
+          ],
+        )));
   }
 }
